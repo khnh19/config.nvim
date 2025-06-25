782452445053583c3d84f648a48814cea2172e8c
Khanh Nguyen <khnhn1901@gmail.com>
2025-06-25 16:56:35 +0700

    feat: remove `bufferline.nvim`

--- lua/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'akinsho/bufferline.nvim',
-  lazy = false,
-  opts = {},
-  keys = {
-    { '<leader>l', vim.cmd.BufferLineCycleNext },
-    { '<leader>h', vim.cmd.BufferLineCyclePrev },
-  },
-}
