7e3f3379ad5b28d8d9ac15b77301a7f21677b05d
Khanh Nguyen <khnhn1901@gmail.com>
2025-06-25 10:48:18 +0700

    feat: add keymap for no highlight

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -8,5 +8,7 @@ map('n', '<leader>q', '<cmd>q<cr>')
 map('n', '<leader>w', '<cmd>w<cr>')
 map('n', '<leader>x', '<cmd>x<cr>')
 
+map('n', '<leader>nh', ':nohl<cr>')
+
 map('n', 'j', [[v:count?'j':'gj']], { noremap = true, expr = true })
 map('n', 'k', [[v:count?'k':'gk']], { noremap = true, expr = true })
